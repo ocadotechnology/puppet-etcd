4bc608db64b56c51c736f8a4857c08078a35f8ca
Marius Berntsberg <marius@redpill-linpro.com>
2015-02-16 11:58:39 +0100

    move config file to fit debian defaults and make upstart read defaults

--- manifests/config.pp
+++ manifests/config.pp
@@ -12,20 +12,12 @@ class etcd::config {
       }
     }
     'Debian' : {
-      file { '/etc/etcd':
-        ensure => directory,
-        owner  => $etcd::user,
-        group  => $etcd::group,
-        mode   => '0555'
-      }
-
-      file { '/etc/etcd/etcd.conf':
+      file { '/etc/default/etcd':
         ensure  => file,
         owner   => $etcd::user,
         group   => $etcd::group,
         mode    => '0644',
-        content => template('etcd/etcd.conf.erb'),
-        require => File['/etc/etcd']
+        content => template('etcd/etcd.default.erb'),
       }
     }
     default  : {
